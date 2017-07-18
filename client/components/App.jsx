3947dbde3402e8d63f56ec952a87862456952b5d
Keshava Bhamidipaty <kesh.bhamidipaty@gmail.com>
2017-07-17 17:15:36 -0700

    commit before installing react-toolbox and prereqs

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  
+  render(){
+    return(
+      <div>
+        <header>
+          <h1>Heading</h1>
+          <hr />
+        </header>
+        <div>
+          <button>Btn1</button>
+          <button>Btn2</button>
+          <button>Btn3</button>
+        </div>
+      </div>
+      );
+  }
+}
