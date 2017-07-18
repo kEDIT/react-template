a65c896f1db8eba483a370afabbe324ae90d3b86
Keshava Bhamidipaty <kesh.bhamidipaty@gmail.com>
2017-07-17 20:42:23 -0700

    webpack-dev-server, postcss, react-css-themr, & react-toolbox installed & running

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,10 @@ module.exports = {
     },
     module: {
         loaders: [
+            { test: /\.json$/, loader: "json-loader"},
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loaders: ['style-loader',{ loader: 'css-loader',options: { importLoaders: 1} },'postcss-loader']},
         ]
     },
     plugins: [HtmlWebpackPluginConfig]
